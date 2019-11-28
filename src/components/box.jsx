cb1b36d3c4e717217198145c2350574752bb847f
funkylen <funkylen@gmail.com>
2019-11-28 07:56:59 +0300

    Добавление redux

---
+++ src/components/box.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Box extends React.Component {
+    render() {
+        return (
+            <div className="box">
+                <button>Click!</button>
+            </div>
+        )
+    }
+}
+
+export default Box;
