e855be1e930da581c2dd8e83d8b42a4290230b0d
Lyashka <iuliana9864@gmail.com>
2019-12-04 01:46:54 +0300

    Добавление адаптивности странице добавления категории

--- src/components/Account/addCategory.js
+++ src/components/Account/addCategory.js
@@ -4,13 +4,13 @@ import React from 'react';
 class addCategory extends React.Component {
   render() {
     return (
-      <div className="card">
+      <div className="card mt-1">
         <div className="card-body">
           <h5 className="card-title mb-4">Добавление категории</h5>
 
           <div className="col-sm mb-4">
             <label>Название категории</label>
-            <input type="text" className="form-control" placeholder="Название категории" style={{ width: "300px" }} />
+            <input type="text" className="form-control" placeholder="Название категории" style={{ maxWidth: "300px", minWidth: "100px" }} />
           </div>
 
           <div className="col-sm">
