2f747d491f61dc74ab7a765bfffbec7a66286309
Lyashka <iuliana9864@gmail.com>
2019-12-05 15:44:28 +0300

    Изменение кнопки загрузки изображения

--- src/components/Account/AddProduct.js
+++ src/components/Account/AddProduct.js
@@ -1,43 +1,42 @@
 import React from 'react';
 
 class AddProduct extends React.Component {
-	render() {
-		return (
-			<div className="card mt-1">
-				<div className="card-body">
-					<h5 className="card-title mb-1">Добавление товара</h5>
-				</div>
-      <form>
-				<div className="row pl-3 pr-3 mb-3">
-					<div className="col-sm">
-						<label>Название товара</label>
-						<input type="text" className="form-control mb-2" placeholder="Название товара" />
-						<label>Цена товара</label>
-						<input type="number" className="form-control" placeholder="Цена товара" />
-					</div>
+  render() {
+    return (
+      <div className="card mt-1">
+        <div className="card-body">
+          <h5 className="card-title mb-1">Добавление товара</h5>
+        </div>
+        <form>
+          <div className="row pl-3 pr-3 mb-3">
+            <div className="col-sm">
+              <label>Название товара</label>
+              <input type="text" className="form-control mb-2" placeholder="Название товара" />
+              <label>Цена товара</label>
+              <input type="number" className="form-control" placeholder="Цена товара" />
+            </div>
 
-					<div className="col-sm">
-						<label>Описание товара</label>
-						<textarea className="form-control" rows="4" style={{ resize: 'none' }} />
-					</div>
-				</div>
+            <div className="col-sm">
+              <label>Описание товара</label>
+              <textarea className="form-control" rows="4" style={{ resize: 'none' }} />
+            </div>
+          </div>
 
-				<div className="col-sm mb-3">
-					<button type="button" className="btn btn-secondary">
-						Загрузить изображение товара
+          <div className="col-sm mb-3 mt-4 ml-3" style={{width: "400px"}}>
+            <input type="file" class="custom-file-input" />
+            <label class="custom-file-label">Выберите изображение для товара</label>
+          </div>
+
+          <div className="col-sm mb-3">
+            <button type="button" className="btn btn-danger">
+              Добавить
 					</button>
-				</div>
+          </div>
 
-				<div className="col-sm mb-3">
-					<button type="button" className="btn btn-danger">
-						Добавить
-					</button>
-				</div>
-
-      </form>
-			</div>
-		);
-	}
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AddProduct;
